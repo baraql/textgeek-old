eaaa8f23746d4487544329a31267b6907c4c597c
baraql <baraqlip@gmail.com>
2023-03-12 16:04:59 -0700

    tried to do file uploading

--- src/App.js
+++ src/App.js
@@ -1,142 +1,59 @@
-import React, { useState, useEffect, useRef } from "react";
-import "./styles.css";
-import initSqlJs from "sql.js";
-import Homepage from "./Homepage";
-
-// Required to let webpack 4 know it needs to copy the wasm file to our assets
+import React, { useState, useEffect, useRef } from 'react';
+import initSqlJs from 'sql.js';
 import sqlWasm from "!!file-loader?name=sql-wasm-[contenthash].wasm!sql.js/dist/sql-wasm.wasm";
 
 export default function App() {
-  const [db, setDb] = useState(null);
-  const [error, setError] = useState(null);
-  const inputFile = useRef(null)
+    // State variables
+    const [db, setDb] = useState(null);
+    const [error, setError] = useState(null);
+    const [selectResult, setSelectResult] = useState(null);
+    // State or Ref for input file
+    const inputFile = useRef(null);
 
-  useEffect(async () => {
-    // sql.js needs to fetch its wasm file, so we cannot immediately instantiate the database
-    // without any configuration, initSqlJs will fetch the wasm files directly from the same path as the js
-    // see ../craco.config.js
-    if (inputFile) {
-      try {
-        const SQL = await initSqlJs({ locateFile: () => sqlWasm });
-        setDb(new SQL.Database());
-      } catch (err) {
-        setError(err);
-      }
-    }
-  }, []);
+    // function to handle file upload
+    const handleFileUpload = async (event) => {
+        console.log("handleFileUpload");
+        inputFile.current = await event.target.files[0];
+        try {
+            const SQL = await initSqlJs({ locateFile: () => sqlWasm });
+            if (inputFile && inputFile.current) {
+                // load the file and create a new database
+                const file = inputFile.current;
+                const fileReader = new FileReader();
+                fileReader.onload = function () {
+                    const Uints = new Uint8Array(this.result);
+                    setDb(new SQL.Database(Uints));
+                }.bind(setDb);
+                fileReader.readAsArrayBuffer(file);
 
-  const handleFileUpload = (event) => {
-    console.log("handleFileUpload");
-    inputFile.current = event.target.files[0]; // update the inputFile ref object
-    const reader = new FileReader();
-    reader.onload = () => {
-      const f = event.target.files[0];
-      const r = new FileReader();
-      r.onload = function () {
-        const Uints = new Uint8Array(r.result);
-        const db = new SQL.Database(Uints);
-      }
-      r.readAsArrayBuffer(f);
-    }
-  }
-
-  // if (error) return <pre>{error.toString()}</pre>;
-  // else if (!db) return <pre>Loading...</pre>;
-  // else return <SQLRepl db={db} />;
-  // return <Homepage />;
-
-  // This should go in its own function
-  if (inputFile.current === useRef(null).current) return <ChooseScreen handleFileUpload={handleFileUpload} inputFile={inputFile} />
-  else return(
-    nice
-    );
-}
-
-function ChooseScreen({handleFileUpload, inputFile}) {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>TextGeek</p>
-        <p>Choose your chat.db file.</p>
-        {/* Change this to react-native-fs instead of file upload */}
-        <input type="file" onChange={(event) => handleFileUpload(event)} />
-
-      </header>
-    </div>
-  );
-}
-
-/**
- * A simple SQL read-eval-print-loop
- * @param {{db: import("sql.js").Database}} props
- */
-function SQLRepl({ db }) {
-  const [error, setError] = useState(null);
-  const [results, setResults] = useState([]);
-
-  function exec(sql) {
-    try {
-      // The sql is executed synchronously on the UI thread.
-      // You may want to use a web worker here instead
-      setResults(db.exec(sql)); // an array of objects is returned
-      setError(null);
-    } catch (err) {
-      // exec throws an error when the SQL statement is invalid
-      setError(err);
-      setResults([]);
-    }
-  }
-
-  return (
-    <div className="App">
-      <h1>React SQL interpreter</h1>
-
-      <textarea
-        onChange={(e) => exec(e.target.value)}
-        placeholder="Enter some SQL. No inspiration ? Try “select sqlite_version()”"
-      ></textarea>
-
-      <pre className="error">{(error || "").toString()}</pre>
-
-      <pre>
-        {
-          // results contains one object per select statement in the query
-          results.map(({ columns, values }, i) => (
-            <ResultsTable key={i} columns={columns} values={values} />
-          ))
+                handleSelect();
+                console.log(selectResult);
+            }
+        } catch (err) {
+            // if err.toString() == ""
+            setError(err);
         }
-      </pre>
-    </div>
-  );
-}
+    };
 
-/**
- * Renders a single value of the array returned by db.exec(...) as a table
- * @param {import("sql.js").QueryExecResult} props
- */
-function ResultsTable({ columns, values }) {
-  return (
-    <table>
-      <thead>
-        <tr>
-          {columns.map((columnName, i) => (
-            <td key={i}>{columnName}</td>
-          ))}
-        </tr>
-      </thead>
-
-      <tbody>
-        {
-          // values is an array of arrays representing the results of the query
-          values.map((row, i) => (
-            <tr key={i}>
-              {row.map((value, i) => (
-                <td key={i}>{value}</td>
-              ))}
-            </tr>
-          ))
+    // function to handle select statement
+    const handleSelect = () => {
+        // e.preventDefault();
+        if (db) {
+            console.log("handleSelect");
+            // perform a select statement
+            const selectResult = db.exec("SELECT * FROM chat;");
+            setSelectResult(selectResult);
         }
-      </tbody>
-    </table>
-  );
+    }
+
+    // render the component
+    if (error) return <pre>{error.toString()}</pre>;
+    else if (!db) {
+        return (
+            <div>
+                <p>Choose a .db file:</p>
+                <input type="file" onChange={handleFileUpload} accept=".db" />
+            </div>
+        );
+    }
 }
